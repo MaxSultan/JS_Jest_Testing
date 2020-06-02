d3ba7023673f09c5cad8c9ff52992ee6b19b6f2c
Max Sultan <maxsultakent@gmail.com>
2020-06-02 17:10:18 -0600

    finished all tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,20 @@
 import {
   // methods,
   // methods,
+  sortArtistByName,
+  findObjByName,
+  numberOfLikes,
+  justArtistMoreThan100Likes,
+  moreThan100Likes,
+  doubleLikes,
+  sortArray,
+  findItem,
+  evensOnlyAndDoubleArray,
+  someNumbersAreOdd,
+  allNumbersGreaterThanZero,
+  arraySum,
+  evensOnlyArray,
+  doubleArray,
   originalArray
 } from '../app'
 
@@ -8,5 +22,137 @@ import {
 describe('originalArray Test', ()=> {
   test('originalArray should return it self', ()=>{
     expect(originalArray([1,2,3,4])).toEqual([1,2,3,4])
+  });
+});
+
+describe('double array',() => {
+test('numbers should be doubled', ()=>{
+  expect(doubleArray([1,2,3,4])).toEqual([2,4,6,8]);
+  });
+});
+
+describe('evens only array', ()=> {
+  test('filters out odd numbers', ()=>{
+    expect(evensOnlyArray([1,2,3,4])).toEqual([2,4])
+  });
+  test('correctly handles negative numbers', ()=>{
+    expect(evensOnlyArray([-1,-2,-3,-4])).toEqual([-2,-4])
+  });
+});
+
+describe('sum array', () =>{
+  test('return the sum of the array', ()=>{
+    expect(arraySum([1,2,3,4])).toBe(10);
+  });
+  test('return a sum of an array with negative numbers', ()=>{
+    expect(arraySum([-1,2,3,4])).toBe(8);
+  });
+  test('return a sum of an array with decimals', ()=>{
+    expect(arraySum([1.1,2,3.1,4])).toBe(10.2);
+  });
+});
+
+describe('numbers greater than zero', ()=>{
+  test('returns true if all numbers are greater than zero', function(){
+    expect(allNumbersGreaterThanZero([1,2,3,4])).toBe(true);
+  });
+  test('returns false if a number is less than 0', ()=>{
+    expect(allNumbersGreaterThanZero([1,2,-3,4])).toBe(false);
+  });
+});
+
+describe('checks if odd numbers are present', () => {
+  test('returns true if an odd value is present', () => {
+    expect (someNumbersAreOdd([1,2,3,4])).toBe(true);
+  });
+  test('returns false if only even values are present', () => {
+    expect(someNumbersAreOdd([2,4,6,8])).toBe(false);
+  });
+});
+
+describe('doubles numbers and returns the even ones', () => {
+  test('doubles numbers and returns the even ones', ()=> {
+    expect(evensOnlyAndDoubleArray([1,2,3,4])).toEqual([4,8]);
+  });
+  test('returns negative even numbers doubled', () => {
+    expect(evensOnlyAndDoubleArray([-2,-4,-6])).toEqual([-4,-8,-12])
   })
-})
+});
+
+describe('returns the value of the requested item if contained in array', () =>{
+  test('returns the value of 2 when array contains 2', () => {
+    expect(findItem([1,2,3,4],2)).toBe(2);
+  });
+  test('returns "not found" when 1 is not found', () => {
+    expect(findItem([2,4,6,8],1)).toEqual('not found');
+  });
+});
+
+describe('sorts an array in descending order', () => {
+  test('returns array in descending order', () => {
+    expect(sortArray([1,2,3,4])).toEqual([4,3,2,1]);
+  });
+  test('returns an array with negative numbers in descending order', () => {
+    expect(sortArray([-10,-1,-100, -1000])).toEqual([-1,-10,-100,-1000]);
+  });
+  test('returns an array of decimals in descending order', () => {
+    expect (sortArray([1.1, 1.13, 1.4, 1.009])).toEqual([1.4,1.13,1.1,1.009])
+  })
+});
+
+let artists = [
+  {name: 'a', likes:100},
+  {name: 'b', likes:99},
+  {name: 'c', likes:101}, 
+]
+
+describe("doubles the artist's likes", () => {
+  test('likes * 2', () => {
+    expect(doubleLikes(artists)).toEqual([
+      {name: 'a', likes:200},
+      {name: 'b', likes:198},
+      {name: 'c', likes:202}, 
+    ]);
+  });
+});
+
+let artists1 = [
+  {name: 'a', likes:100},
+  {name: 'b', likes:99},
+  {name: 'c', likes:101}, 
+]
+
+describe('function returns entire artist hash for artist with over 100 likes', () => {
+  test('artist with more than 100 likes', () => {
+    expect(moreThan100Likes(artists1)).toEqual([{name: 'c', likes: 101}]);
+  });
+});
+
+describe('returns name of artists with over 100 likes', () => {
+  test('artist name with 100+ likes', () => {
+    expect(justArtistMoreThan100Likes(artists1)).toEqual(['c']);
+  });
+ });
+
+ describe('total number of likes', () => {
+   test('total likes equal 300', () => {
+     expect(numberOfLikes(artists1)).toBe(300);
+   });
+ });
+
+ describe('finds objects in an array by name', () => {
+   test('finding artist b in array', () => {
+     expect(findObjByName(artists1, 'b')).toEqual({name: 'b', likes:99})
+   })
+   test('finding artist d in an array', () => {
+     expect(findObjByName(artists1, 'd')).toBe('not found')
+   })
+ })
+
+ describe('sorts array objects by number of likes', () => {
+   test('highest to lowest', () => {
+     expect(sortArtistByName(artists1)).toEqual([ { name: 'c', likes: 101 },
+     { name: 'a', likes: 100 },
+     { name: 'b', likes: 99 } ])
+   })
+ })
--- app.js
+++ app.js
@@ -1,3 +1,156 @@
 export const originalArray = (arr) =>{
   return arr
 }
+
+export const doubleArray = arr => {
+  return arr.map(number => {
+    return number * 2;
+  });
+};
+
+export const evensOnlyArray = arr => {
+  return arr.filter(num => num % 2 == 0);
+};
+
+export function arraySum(numArr){
+  let sum = numArr.reduce((acc, num) => {
+    return acc + num;
+  });
+  return sum;
+};
+
+export function allNumbersGreaterThanZero(numArr){
+  return numArr.every(num => num > 0);
+};
+
+export const someNumbersAreOdd = numArr => {
+  return numArr.some(num => num % 2 === 1);
+};
+
+export function evensOnlyAndDoubleArray(numArr){
+  return numArr.filter(num => num % 2 == 0).map(num => num * 2);
+};
+
+export const findItem = (numArr, num) => {
+  if(numArr.find(number => num == number) === undefined){
+    return 'not found'
+  } else{
+  return numArr.find(number => num == number);
+  }
+};
+
+export function sortArray(numArr){
+  return numArr.sort(function(a, b){return b - a});
+};
+
+// artists = [
+//   {name: 'a', likes:100},
+//   {name: 'b', likes:99},
+//   {name: 'c', likes:101}, 
+// ]
+// // doubles likes of artist
+export function doubleLikes(ArtistsArr){
+  let newArr = ArtistsArr.map((item) => {item.likes *= 2;
+  return item});
+  return newArr;
+ };
+
+ export const moreThan100Likes = artists => {
+  return artists.filter(artist => artist.likes > 100)
+ };
+
+ export function justArtistMoreThan100Likes(arrObj){
+  let famous = [];
+  let over100 = arrObj.filter(artist => artist.likes>100);
+  console.log(over100);
+  over100.forEach(artist => famous.push(artist.name));
+  return famous;
+}
+
+export const numberOfLikes = (artArr) => {
+  return artArr.reduce((a,b) => ({likes: a.likes + b.likes})).likes
+};
+
+export function findObjByName(arr, search){
+  let finder = arr.filter(item => item.name === search);
+    if(finder.length == 1){
+      return finder[0];
+    } else {
+   return 'not found';
+    } 
+  }
+
+  export const sortArtistByName = (artArr) => {
+    return artArr.sort(function(a,b){return b.likes - a.likes})
+  };
+
+
+// EXPORT ALL AS FUNCTIONS AND KEEP THE NAMES THE SAME
+
+// ways to export
+
+// export const methodName = (param) => {
+// }
+
+// ----or---------
+
+// export methodName function(param){
+// }
+
+
+// // ARRAY ITERATOR METHODS
+// numberArray = [1,2,3,4]
+
+// // return array with double values in array
+//doubleArray(numberArray) // => [2,4,6,8];  hint use map 
+
+// // return array with all even numbers
+// evensOnlyArray(numberArray) // => [2,4,6,8];  hint use filter 
+
+// // return array with sum of numbers
+// arraySum(numberArray) // => 10;  hint use reduce
+
+// // return true if every item greater than 0
+// allNumbersGreaterThanZero(numberArray) // => true; use every
+
+// // return true if one or more items in array are odd
+// someNumbersAreOdd(numberArray) // => true; use some
+
+// // return array double and even
+// evensOnlyAndDoubleArray(numberArray) // => [4,8]; // use map and filter
+
+// // find an item method return 'not found' if not found
+// findItem(numberArray, 1) //=> 1
+
+// // sort array desc order
+// sortArray(numberArray) //=> [4,3,2,1] *desc*
+
+
+// // -----BONUS------
+// // ARRAY ITERATOR METHODS ON Objects with name age 
+
+// artists = [
+//   {name: 'a', likes:100},
+//   {name: 'c', likes:99},
+//   {name: 'c', likes:101}, 
+// ]
+// // doubles likes of artist
+// doubleLikes(artists)
+// // => [
+// //   {name: 'a', likes:200},
+// //   {name: 'c', likes:198},
+// //   {name: 'c', likes:202},  
+// // ]; 
+// moreThan100Likes(artists)// =>[ {name: 'c', likes:101} ]; // use filter
+
+// //  return an array of strings of artist name
+// justArtistMoreThan100Likes(artists) //=> ['c']; // use filter
+
+// // get total number of likes fo all artists
+// numberOfLikes(artists) //=> 300; // get all likes
+
+// // find an item method. Returns 'not found' if not found
+// findObjByName(artists, 'a') // => {name: 'a', likes:200}
+
+// // returns array of artist sorted by likes
+// sortArtistByName(artists)
